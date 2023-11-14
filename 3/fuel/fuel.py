5e9dd80c02fa1790ac021f14c1664af74d181a48
Mostafa Piran <piran.m313@gmail.com>
2023-11-15 01:02:51 +0330

    Create fuel.py

---
+++ 3/fuel/fuel.py
@@ -0,0 +1,21 @@
+def main():
+
+    s = get_fraction("Fraction: ")
+    print(s)
+
+def get_fraction(prompt):
+    while True:
+        try:
+            x, y = input(prompt).split("/")
+            if 0 <= int(x)/int(y) <= 0.1:
+                return("E")
+            elif 0.9 <= int(x)/int(y) <= 1:
+                return("F")
+            elif 0.1 < int(x)/int(y) < 0.9:
+                return str(round(int(x)/int(y)*100)) + "%"
+        except (ValueError, ZeroDivisionError):
+            pass
+
+
+if __name__ == "__main__":
+    main()

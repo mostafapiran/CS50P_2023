b936f4a435c9cead8c9a30372d713474b46ef521
Mostafa Piran <piran.m313@gmail.com>
2023-11-15 01:03:06 +0330

    Create game.py

---
+++ 4/game/game.py
@@ -0,0 +1,27 @@
+import random
+
+def main():
+
+    while True:
+        try:
+            level = int(input("Level: "))
+            x = random.randint(1, level)
+            while True:
+                guess = int(input("Guess: "))
+                if guess > x:
+                    print("Too large!")
+                elif guess < x:
+                    print("Too small!")
+                elif guess == x:
+                    print("Just right!")
+                    raise EOFError
+        except ValueError:
+            pass
+        except EOFError:
+            break
+
+
+
+
+if __name__ == "__main__":
+    main()

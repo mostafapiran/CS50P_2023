25621175a3dec258b6a8e319a6ecccf31e5e51df
Mostafa Piran <piran.m313@gmail.com>
2023-11-15 01:03:12 +0330

    Create test_bank.py

---
+++ 5/test_bank/test_bank.py
@@ -0,0 +1,12 @@
+import pytest
+from bank import value
+
+
+def test_greeting():
+    assert value("hello world") == 0
+    assert value("HELLO WORLD") == 0
+    assert value("hi world") == 20
+    assert value("HI WORLD") == 20
+    assert value("wassup world") == 100
+    assert value("WASSUP WORLD") == 100
+    assert value("mostafa") == 100

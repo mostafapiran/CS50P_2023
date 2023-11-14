fbc0c0fadc66be7b1968a8a4468ee37f037dc932
Mostafa Piran <piran.m313@gmail.com>
2023-11-15 01:02:43 +0330

    Create nutrition.py

---
+++ 2/nutrition/nutrition.py
@@ -0,0 +1,30 @@
+f = [
+    {"fruit": "apple", "calories": "130"},
+    {"fruit": "avocado", "calories": "50"},
+    {"fruit": "banana", "calories": "110"},
+    {"fruit": "cantaloupe", "calories": "50"},
+    {"fruit": "grapefruit", "calories": "50"},
+    {"fruit": "grapes", "calories": "90"},
+    {"fruit": "honeydew melon", "calories": "50"},
+    {"fruit": "kiwifruit", "calories": "90"},
+    {"fruit": "lemon", "calories": "15"},
+    {"fruit": "lime", "calories": "20"},
+    {"fruit": "nectarine", "calories": "60"},
+    {"fruit": "orange", "calories": "80"},
+    {"fruit": "peach", "calories": "60"},
+    {"fruit": "pear", "calories": "100"},
+    {"fruit": "pineapple", "calories": "50"},
+    {"fruit": "plums", "calories": "70"},
+    {"fruit": "strawberries", "calories": "50"},
+    {"fruit": "sweet cherries", "calories": "100"},
+    {"fruit": "tangerine", "calories": "50"},
+    {"fruit": "watermelon", "calories": "80"}
+]
+
+
+
+
+x = input("Input: ").strip().lower()
+for i in f:
+    if x == i["fruit"]:
+        print(f"Calories: {i['calories']}")
